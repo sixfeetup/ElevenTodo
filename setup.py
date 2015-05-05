ddc62255fe30037c5c4e2a9a6649042f5ca653cb
Christian Long <christianzlong@gmail.com>
2015-05-05 15:39:58 -0500

    Initial package from pcreate alchemy scaffold

---
+++ setup.py
@@ -0,0 +1,47 @@
+import os
+
+from setuptools import setup, find_packages
+
+here = os.path.abspath(os.path.dirname(__file__))
+with open(os.path.join(here, 'README.txt')) as f:
+    README = f.read()
+with open(os.path.join(here, 'CHANGES.txt')) as f:
+    CHANGES = f.read()
+
+requires = [
+    'pyramid',
+    'pyramid_chameleon',
+    'pyramid_debugtoolbar',
+    'pyramid_tm',
+    'SQLAlchemy',
+    'transaction',
+    'zope.sqlalchemy',
+    'waitress',
+    ]
+
+setup(name='pyramid_todo',
+      version='0.0',
+      description='pyramid_todo',
+      long_description=README + '\n\n' + CHANGES,
+      classifiers=[
+        "Programming Language :: Python",
+        "Framework :: Pyramid",
+        "Topic :: Internet :: WWW/HTTP",
+        "Topic :: Internet :: WWW/HTTP :: WSGI :: Application",
+        ],
+      author='',
+      author_email='',
+      url='',
+      keywords='web wsgi bfg pylons pyramid',
+      packages=find_packages(),
+      include_package_data=True,
+      zip_safe=False,
+      test_suite='pyramid_todo',
+      install_requires=requires,
+      entry_points="""\
+      [paste.app_factory]
+      main = pyramid_todo:main
+      [console_scripts]
+      initialize_pyramid_todo_db = pyramid_todo.scripts.initializedb:main
+      """,
+      )
