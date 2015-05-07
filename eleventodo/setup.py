35c27a58295f96c5aaca1ea4819a09285b3844dc
Christian Long <christianzlong@gmail.com>
2015-05-07 14:14:20 -0500

    Add requirements
    Note: I have to do `python setup.py develop` instead of `pip install -e .`.
    However,  `python setup.py develop` installs Pyramid 1.6a1, and I want 1.5.7.
    So, I specify pyramid~=1.5.7 in setup.py, then install, then pip freeze >
    requirements.txt.

---
+++ eleventodo/setup.py
@@ -0,0 +1,47 @@
+import os
+
+from setuptools import setup, find_packages
+
+here = os.path.abspath(os.path.dirname(__file__))
+with open(os.path.join(here, 'README.txt')) as f:
+    README = f.read()
+with open(os.path.join(here, 'CHANGES.txt')) as f:
+    CHANGES = f.read()
+
+requires = [
+    'pyramid~=1.5.7',
+    'pyramid_chameleon',
+    'pyramid_debugtoolbar',
+    'pyramid_tm',
+    'SQLAlchemy',
+    'transaction',
+    'zope.sqlalchemy',
+    'waitress',
+    ]
+
+setup(name='eleventodo',
+      version='0.0',
+      description='eleventodo',
+      long_description=README + '\n\n' + CHANGES,
+      classifiers=[
+        "Programming Language :: Python",
+        "Framework :: Pyramid",
+        "Topic :: Internet :: WWW/HTTP",
+        "Topic :: Internet :: WWW/HTTP :: WSGI :: Application",
+        ],
+      author='',
+      author_email='',
+      url='',
+      keywords='web wsgi bfg pylons pyramid',
+      packages=find_packages(),
+      include_package_data=True,
+      zip_safe=False,
+      test_suite='eleventodo',
+      install_requires=requires,
+      entry_points="""\
+      [paste.app_factory]
+      main = eleventodo:main
+      [console_scripts]
+      initialize_eleventodo_db = eleventodo.scripts.initializedb:main
+      """,
+      )
